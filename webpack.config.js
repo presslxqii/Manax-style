2a36883ba7ed7a62a938792991a3c5057fe0b3c7
Maksim Berdnikov <presslxqii@yandex.ru>
2023-02-21 12:31:47 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+	entry: {
+		components: './src/pages/Components/index.js',
+		trade: './src/pages/Trade/index.js'
+	},
+	mode: 'development',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader', 'postcss-loader'],
+			},
+		],
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{ from: 'src/assets', to: 'assets' },
+			],
+		}),
+		new HtmlWebpackPlugin({
+			template: 'src/index.html',
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'components.html',
+			template: 'src/pages/Components/index.html',
+			chunks: ['components'],
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'trade.html',
+			template: 'src/pages/Trade/index.html',
+			chunks: ['trade'],
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].bundle.css'
+		})
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'dist'),
+		},
+		hot: true,
+	},
+};
